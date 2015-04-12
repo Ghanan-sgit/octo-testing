062f92defde2b0f4c4c1a73f6e9160e0c29ba84a
sachithrangana <sachithr7@gmail.com>
2015-04-12 12:09:13 -0700

    Sachith

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('abc', 'abcController@index');
+
 Route::get('prj', 'HomeContoller@index');
 
 Route::controllers([
---
+++ resources/views/abc.blade.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+<title>dsfsfsfd</title>
+</head>
+<body>
+<h1>ssdfdgfg</h1>
+
+
+</body>
+</html>
